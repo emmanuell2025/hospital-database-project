ac7d56994d2171a1c5867207575dfae9b3db2496
JIsom223 <57500589+JIsom223@users.noreply.github.com>
2019-11-30 20:27:36 -0500

    Update Appointment.sql

--- Appointment.sql
+++ Appointment.sql
@@ -1,4 +1,4 @@
 SELECT apID, appointment.DeptID, apDesc, procID, roomNum, apDate, apTime, pMRN
 FROM appointment, doctor
 WHERE appointment.drID = doctor.drID
-AND doctor.drID = ' '
+AND doctor.drID = ' ';
