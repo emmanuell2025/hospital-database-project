e154cc4f530f861f93fcf9ba7370e8cc49890a5a
JIsom223 <57500589+JIsom223@users.noreply.github.com>
2019-11-30 22:23:23 -0500

    Lists doctors appointments

--- Appointment.sql
+++ Appointment.sql
@@ -1,4 +1,6 @@
-SELECT apID, appointment.DeptID, apDesc, procID, roomNum, apDate, apTime, pMRN
-FROM appointment, doctor
-WHERE appointment.drID = doctor.drID
+SELECT patient.pMRN, patient.pFName, patient.pLName, apID, apDesc, appointment.procID, roomNum, apDate, DATE_FORMAT(`appointment`.`apTime`,'%h:%i %p')
+FROM appointment, doctor, patient
+WHERE patient.pMRN = appointment.pMRN
+AND patient.procID = appointment.procID
+AND doctor.DeptID = appointment.DeptID
 AND doctor.drID = ' ';
