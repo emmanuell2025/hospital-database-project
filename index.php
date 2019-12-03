4bce258192f2d1c1f6ccb23a7c690aaad6318cc6
emmanuell2025 <emmanuell2025@gmail.com>
2019-12-03 16:21:14 -0500

    Added email Fixed appointment list

--- index.php
+++ index.php
@@ -16,6 +16,11 @@ include "templates/header.php";
         <a href="reception/reception_login.php"><strong>Reception</strong></a> Login
       </li>
     </ul>
+
+    <a href="mailto:elopez1@radar.gsw.edu">Emmanuel Lopez</a>
+    <a href="mailto:mchannel@radar.gsw.edu">Morgan Channell</a>
+    <a href="mailto:jisom1@radar.gsw.edu">James Isom</a>
+    <a href="mailto:mdunnava@radar.gsw.edu">Madison Dunnava</a>
   </body>
 </html>
 
