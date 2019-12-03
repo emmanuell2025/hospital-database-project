3205a4dd8f24949c22adfa565548aaa17461c6e8
JIsom223 <57500589+JIsom223@users.noreply.github.com>
2019-12-03 16:05:25 -0500

    fixed appointments

--- Patient View Appointments.sql
+++ Patient View Appointments.sql
@@ -1,7 +1,5 @@
 SELECT apDate, apTime, apDesc, roomNum, drLName, procCost 
 FROM appointment, doctor, proc
-WHERE appointment.procID = proc.procID 
-AND doctor.drID = appointment.drID 
-AND doctor.drID = proc.drID 
+WHERE doctor.drID = appointment.drID 
 AND appointment.pMRN = proc.pMRN
 AND appointment.pMRN = ' ';
