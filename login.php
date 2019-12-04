8a11f3a3b67f31d4b286f1cbf81aa7debda996bc
Emmanuel Lopez <emmanuell2025@gmail.com>
2019-12-04 13:46:45 -0500

    fixed doctorID session

--- login.php
+++ login.php
@@ -4,6 +4,7 @@ session_start();
 
 if ($_SESSION['loginID']=='doctor'){
   $_SESSION["user"] = $_POST['doctorID'];
+  $_SESSION["doctorID"] = $_POST['doctorID'];
   echo "doctor";
   header("Refresh:0; url=./patient/patient_list.php");
 }
